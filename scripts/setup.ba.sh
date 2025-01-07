b48d8b4e5c7a4b3ec6dfcb4526ee65a7c47dda3b
JC Guerrero <JCystems@gMail.com>
2025-01-07 16:41:23 -0600

    ff

--- scripts/setup.ba.sh
+++ scripts/setup.ba.sh
@@ -6,13 +6,17 @@ scripts_path=./scripts
 
 set -x
 
+# pip > pipx
 bash ${scripts_path}/pip/upgrade.ba.sh
 
+# pipx > uv
 bash ${scripts_path}/pipx/install.ba.sh
 
+# dependencies & tools
 bash ${scripts_path}/uv/tool/install.ba.sh
 bash ${scripts_path}/uv/sync.ba.sh
 
+# jupyter .venv
 bash ${scripts_path}/ipykernel/install.ba.sh
 
 set +x
